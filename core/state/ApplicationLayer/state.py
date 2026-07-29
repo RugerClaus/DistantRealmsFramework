6e2dfbe18bb931dc640ae8b4c8b8524ae26cf716
rugerclaus <rogerrock12games@gmail.com>
2026-07-29 08:48:27 -0600

    settled APP_STATE

--- core/state/ApplicationLayer/state.py
+++ core/state/ApplicationLayer/state.py
@@ -1,5 +1,5 @@
 from enum import Enum,auto
 
 class APP_STATE(Enum):
-    NOT_LOADED = auto()
-    LOADED = auto()
+    RUNNING = auto()
+    FROZEN = auto()
--- core/state/ApplicationLayer/statemanager.py
+++ core/state/ApplicationLayer/statemanager.py
@@ -5,11 +5,11 @@ from core.state.basestatemanager import BaseStateManager
 class AppStateManager(BaseStateManager):
     def __init__(self):
         allowed_transitions = {
-            APP_STATE.LOADED: [APP_STATE.NOT_LOADED,],
-            APP_STATE.NOT_LOADED: [APP_STATE.LOADED],
+            APP_STATE.RUNNING: [APP_STATE.FROZEN,],
+            APP_STATE.FROZEN: [APP_STATE.RUNNING],
         }
         super().__init__(
-            initial_state=APP_STATE.NOT_LOADED,
+            initial_state=APP_STATE.RUNNING,
             allowed_transitions=allowed_transitions,
             log_fn=lambda old, new, state_type: log_state_transition(old, new, state_type),
             state_name="APP_STATE",
