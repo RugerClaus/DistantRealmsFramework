7d8dcbf17222e2d37e9ee1628affbd7c23b6c183
rugerclaus <rogerrock12games@gmail.com>
2026-08-06 06:48:31 -0600

    major usability improvments for command system.

--- core/guts/input/CommandModule.py
+++ core/guts/input/CommandModule.py
@@ -1,33 +1,32 @@
 #will integrate keys class later
 from core.guts.input.keys import Keys
-import pygame
 
 class CommandModule:
-    def __init__(self):
+    def __init__(self,eventmanager):
+        self.eventmanager = eventmanager
         self.keys = Keys()
         self.sequences = {
-            "debug": [pygame.K_F9],
-            "secret": [pygame.K_s, pygame.K_e, pygame.K_c, pygame.K_r, pygame.K_e, pygame.K_t],
-            "developer": [pygame.K_F2],
-            "fuck": [pygame.K_f,pygame.K_u,pygame.K_c,pygame.K_k],
-            "musicon": [pygame.K_m,pygame.K_u,pygame.K_s,pygame.K_i,pygame.K_c,pygame.K_o,pygame.K_n],
-            "musicoff": [pygame.K_m,pygame.K_u,pygame.K_s,pygame.K_i,pygame.K_c,pygame.K_o,pygame.K_f,pygame.K_f],
-            "monitor_system_states": [pygame.K_F8,pygame.K_1],
-            "monitor_runtime_states": [pygame.K_F8,pygame.K_2],
-            "monitor_application_states": [pygame.K_F8,pygame.K_3],
-            "monitor_all_states": [pygame.K_F8,pygame.K_4],
-            "raise_opacity": [pygame.K_F8,pygame.K_5],
-            "lower_opacity": [pygame.K_F8,pygame.K_6],
-            "reload_ui": [pygame.K_F1,pygame.K_1],
-            "reload_application": [pygame.K_F1,pygame.K_2]
+            "debug": [self.keys.F9_key()],
+            "secret": [self.keys.s_key(), self.keys.e_key(), self.keys.c_key(), self.keys.r_key(), self.keys.e_key(), self.keys.t_key()],
+            "developer": [self.keys.F2_key()],
+            "monitor_system_states": [self.keys.F8_key(),self.keys.one_key()],
+            "monitor_runtime_states": [self.keys.F8_key(),self.keys.two_key()],
+            "monitor_application_states": [self.keys.F8_key(),self.keys.three_key()],
+            "monitor_all_states": [self.keys.F8_key(),self.keys.four_key()],
+            "raise_opacity": [self.keys.F8_key(),self.keys.five_key()],
+            "lower_opacity": [self.keys.F8_key(),self.keys.six_key()],
+            "reload_ui": [self.keys.F1_key(),self.keys.one_key()],
+            "reload_application": [self.keys.F1_key(),self.keys.two_key()],
+            "reload_menu_editor": [self.keys.F1_key(),self.keys.three_key()],
+            "reload_form_editor": [self.keys.F1_key(),self.keys.four_key()]
         }
         self.buffer = []
         self.buffer_timer = 0
         self.buffer_timeout = 5000
 
     def update(self, event):
-        if event.type == pygame.KEYDOWN:
-            now = pygame.time.get_ticks()
+        if event.type == self.eventmanager.keydown():
+            now = self.eventmanager.system.time.get_current_time()
 
             if now - self.buffer_timer > self.buffer_timeout:
                 self.buffer.clear()
