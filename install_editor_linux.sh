05edfca0127268eae2a4e0dc2f90cc8790e1b782
rugerclaus <rogerrock12games@gmail.com>
2026-08-09 09:40:10 -0600

    updated editor installer

--- install_editor_linux.sh
+++ install_editor_linux.sh
@@ -11,3 +11,12 @@ unzip -d tools DR_Editor_Linux.zip
 
 echo "package installed"
 rm DR_Editor_Linux.zip
+
+read -r -p "Would you like to run the editor now? [y/N] " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    echo "opening editor..."
+    ./run_editor.sh
+else
+    echo "run ```bash run_editor.sh``` to start the editor"
+fi
