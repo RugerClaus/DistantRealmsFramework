3a6b56387af95eb57dcd8edd9acbff3325bfebe1
rugerclaus <rogerrock12games@gmail.com>
2026-08-12 06:42:20 -0600

    updating example.config.py and added framework version to it

--- example.config.py
+++ example.config.py
@@ -1,16 +1,18 @@
 import platform
 
 config = {
-    "TITLE": "My New Application",
-    "AUTHORS": [""],
-    "VERSION": "0.6.0-alpha",
+    "TITLE": "My App",
+    "AUTHORS": ["RugerClaus"],
+    "VERSION": "yourversionsystem",
+    "FRAMEWORK VERSION": "0.6.0-alpha",
     "OS": platform.system(),
     "OSV": platform.version(),
     "OSR": platform.release(),
     "UPDATE_ZIP_NAME": "",
-    "UPDATER_WINDOWS": "",
-    "UPDATER_LINUX": "",
-    "UPDATER_VERSION": "",
+    "UPDATER_WINDOWS": "updater.exe",
+    "UPDATER_LINUX": "updater",
+    "UPDATER_VERSION": "0.0.2",
+    "NSTATURL": "https://snowblitz.net",
     "API_KEY": "",
     "API": {
         "LEADERBOARD": "",
